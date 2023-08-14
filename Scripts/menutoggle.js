d95277bc93eec7bf187edd26db0a47dba02af632
shanecorless23 <139636921+shanecorless23@users.noreply.github.com>
2023-08-14 22:01:51 +0100

    first post

---
+++ Scripts/menutoggle.js
@@ -0,0 +1,16 @@
+function displayBreakfast() {
+  var x = document.getElementById("breakfast");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function displayRocketCoffee() {
+  var x = document.getElementById("rocketCoffee");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
